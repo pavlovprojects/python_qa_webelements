10cd3f4df3f6d73a9210bd75089afae896ef80cb
QA Automation <44262112+konflic@users.noreply.github.com>
2019-08-21 22:33:25 +0300

    Update webelement.py

--- 1_webelement/webelement.py
+++ 1_webelement/webelement.py
@@ -7,7 +7,7 @@ firefox.get("https://yandex.ru")
 home_tabs = firefox.find_element_by_css_selector(".home-tabs")
 search_input = firefox.find_element_by_css_selector("input#text")
 
-# for i in dir(home_tabs):
-#     print(i)
+for i in dir(home_tabs):
+  print(i)
 
 firefox.close()
